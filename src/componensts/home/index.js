0de9ee72b561fa4a65b2465c966a62923d4b49a7
Jan-Egil-Staff-Ovretveit <janstovr@icloud.com>
2023-02-01 13:19:31 +0100

    Added login and register buttons in view

--- src/componensts/home/index.js
+++ src/componensts/home/index.js
@@ -3,6 +3,12 @@ import classes from "./index.module.css";
 function StartingPageContent() {
   return (
     <div className={classes["landing-page-background"]}>
+
+      <h1 className={classes["title"]}>Kjærligheten</h1>
+      <div className={classes['btn-group']}>
+        <button className={classes['btn-signup']}>Opprett konto</button>
+        <button className={classes['btn-login']}>Logg inn</button>
+      </div>
     </div> 
   );
 }
