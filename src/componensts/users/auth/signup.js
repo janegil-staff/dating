cc997a13cb694cd996ed9239bcf6c97b18f3e9fc
Jan-Egil-Staff-Ovretveit <janstovr@icloud.com>
2023-02-01 17:40:29 +0100

    Add reusable cient side validation to form inouts

--- src/componensts/users/auth/signup.js
+++ src/componensts/users/auth/signup.js
@@ -1,19 +1,97 @@
+import useInput from "@/hooks/use-input";
 import classes from "./signup.module.css";
 const Signup = (props) => {
+  const {
+    value: enteredEmail,
+    isValid: enteredEmailIsValid,
+    hasError: emailInputHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmailInput,
+  } = useInput(value => value.includes("@"));
+
+  const {
+    value: enteredPassword,
+    isValid: enteredPasswordIsValid,
+    hasError: passwordInputHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPasswordInput,
+  } = useInput(value => /^[A-Za-z]\w{7,14}$/.test(value.trim()));
+
+  const {
+    value: enteredPasswordConfirmation,
+    isValid: enteredPasswordIsValidConfirmation,
+    hasError: passwordConfirmationInputHasError,
+    valueChangeHandler: passwordConfirmationChangeHandler,
+    inputBlurHandler: passwordConfirmationBlurHandler,
+    reset: resetPasswordConfirmationInput,
+  } = useInput(value => value === enteredPassword);
+
+  const emailInputClasses = emailInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
+    const passwordInputClasses = passwordInputHasError
+    ? 'form-control invalid'
+    : 'form-control';
+
+    const passwordConfirmationInputClasses = passwordConfirmationInputHasError
+    ? 'form-control invalid'
+    : 'form-control';
 
   return (
     <form>
-      <div className={classes['form-group']}>
+      <div className="form-group">
         <label htmlFor="email">E-post</label>
-        <input className={classes['form-control']} name="email" type="email" placeholde="eksemper@gmail.com" />
+        <input
+          className={emailInputClasses}
+          type="email"
+          id="email"
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+          value={enteredEmail}
+          required
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+        />
+        {emailInputHasError && (
+          <p className={"error-text"}>E-post må inneholde @</p>
+        )}
       </div>
-      <div className={classes['form-group']}>
+      <div className="form-group">
         <label htmlFor="password">Passord</label>
-        <input className={classes['form-control']} name="password" type="password" placeholde="***" />
+        <input
+          className={passwordInputClasses}
+          type="password"
+          id="password"
+          onChange={passwordChangeHandler}
+          onBlur={passwordBlurHandler}
+          value={enteredPassword}
+          required
+          aria-describedby="paswordHelp"
+          placeholder="Velg et pasord"
+        />
+        {passwordInputHasError && (
+          <p className={"error-text"}>Passord må inneholed mellom 8-16 tegn, minst en bokstav</p>
+        )}
       </div>
-      <div className={classes['form-group']}>
-        <label htmlFor="repeatPassword">Gjenta passord</label>
-        <input className={classes['form-control']} name="repeatPassword" type="password" placeholde="***" />
+      <div className="form-group">
+        <label htmlFor="passwordConfirmation">Gjenta passord</label>
+        <input
+          className={passwordConfirmationInputClasses}
+          type="password"
+          id="password"
+          onChange={passwordConfirmationChangeHandler}
+          onBlur={passwordConfirmationBlurHandler}
+          value={enteredPasswordConfirmation}
+          required
+          aria-describedby="paswordHelp"
+          placeholder="Gjenta pasord"
+        />
+        {passwordConfirmationInputHasError && (
+          <p className={"error-text"}>Må være lik dit valgte pasord</p>
+        )}
       </div>
     </form>
   );
