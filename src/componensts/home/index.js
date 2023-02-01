2f94476b8f3ecc25285748b42e8cac21fc19810a
Jan-Egil-Staff-Ovretveit <janstovr@icloud.com>
2023-02-01 10:03:53 +0100

    Addent landingpage background image

--- src/componensts/home/index.js
+++ src/componensts/home/index.js
@@ -1,10 +1,9 @@
-import classes from './index.module.css';
+import classes from "./index.module.css";
 
 function StartingPageContent() {
   return (
-    <section className={classes.landingPageBackground}>
-    <h1>Foobar foo bar</h1>
-    </section>
+    <div className={classes["landing-page-background"]}>
+    </div> 
   );
 }
 
