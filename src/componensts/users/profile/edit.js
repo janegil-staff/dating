aff93d63a906df46c459d548b15f08a62388f51b
Jan-Egil-Staff-Ovretveit <janstovr@icloud.com>
2023-02-04 10:28:54 +0100

    Added file type validation

--- src/componensts/users/profile/edit.js
+++ src/componensts/users/profile/edit.js
@@ -5,18 +5,24 @@ import CropImage from "./image/crop-image";
 import classes from "./image/crop-image.module.css";
 const EditProfile = (props) => {
   const [src, setSrc] = useState(null);
-
+  const [error, setError] = useState(null);
   const handleFileChange = (event) => {
-    if (event.target.files) {
-      const imageSource = URL.createObjectURL(event.target.files[0]);
+    const image = event.target.files[0];
+    if (image) {
+      if (!image.name.match(/\.(jpg|jpeg|png|gif|webp)$/)) {
+        setError("Invalid file type, must be an image file");
+        return;
+      }
+      const imageSource = URL.createObjectURL(image);
       setSrc(imageSource);
     }
   };
 
   return (
     <>
-     <input type="file" onChange={handleFileChange} />
-    <CropImage src={src} setSrc={setSrc} />
+      <input type="file" onChange={handleFileChange} />
+      {error && <p className="error-text">{error}</p>}
+      <CropImage src={src} setSrc={setSrc} />
     </>
   );
 };
